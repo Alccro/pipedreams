4e4a2a81fe090369df1231733be8d49c698a6af7
Alccro <farnelali@gmail.com>
2022-03-25 16:00:57 +0100

    basic create app framework

---
+++ react/pipedreams/src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <>
+      <nav>
+        <div id='navBar'>
+          <NavLink className='nav' to="/">Home</NavLink>
+          <NavLink className='nav' to="/about">About</NavLink>
+          <NavLink className='nav' to="/inspiration">Inspiration</NavLink>
+          <NavLink className='nav' to="/team">Meet the Team</NavLink>
+          <NavLink className='nav' to="/contact">Contact</NavLink>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar
--- react/pipedreams/src/index.js
+++ react/pipedreams/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+import About from './pages/about';
+import Inspiration from './pages/inspiration';
+import Team from './pages/team';
+import Contact from './pages/contact';
+import "./Home.css";
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="about" element={<About />} />
+        <Route path="inspiration" element={<Inspiration />} />
+        <Route path="team" element={<Team />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path='*' element={<main style={{ padding: '1rem'}}><p>Opps, we've sprung a leak. Best to go back while we fix it.</p></main>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
